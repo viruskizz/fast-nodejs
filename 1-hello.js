c1b18afc10005c62883d1bc30316a02d8be186ae
Araiva <kizzaraiva@gmail.com>
2022-02-13 17:19:51 +0700

    first commit

---
+++ 1-hello.js
@@ -0,0 +1,18 @@
+
+// This is basic command
+/*
+    multiple comment
+*/
+console.log('Hello "JS"');
+console.log("This's double quote");
+console.log("this is\t\"JS\"\n");
+console.log("My name " + "is Eve");
+console.log("My name ", "Araiva");
+console.log(10);
+console.log(10 + 10 - 20);
+console.log('Result is ' + (10 + 10 - 20));
+// show your name in command line
+console.log("Araiva");
+console.log("1" + 1);
+console.log(1 + 1);
+console.log('CHANGE');
